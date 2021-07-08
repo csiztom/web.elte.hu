8d10edd4a9b2b75e229bea428854099506cb2ea6
Csizi <tom.csiz@gmail.com>
2021-07-08 22:34:47 +0200

    added basic frontend to eventmaker

--- public/resources/javascript/script.js
+++ public/resources/javascript/script.js
@@ -31,6 +31,9 @@ const Links = {
             <li v-for='l in links'>
                 <a class='file' :href='l.link'>{{ l.name }}</a>
             </li>
+            <li v-for='r in routerLinks'>
+                <router-link class='file' :to='r.link'>{{ r.name }}</router-link>
+            </li>
         </ul>`,
     data: function () {
         return {
@@ -38,7 +41,53 @@ const Links = {
                 {link: 'https://github.com/csiztom/web.elte.hu', name: 'github repo'},
                 {link: '/set', name: 'set - card game'},
                 {link: '/christmas', name: 'christmas chooser (under development)'}
+            ],
+            routerLinks: [
+                {link: '/eventmaker', name: 'event maker'}
             ]
+        }
+    }
+}
+
+const EventMaker = { 
+    template: `
+        <div>
+            <p>eventmaker</p>
+            <p>
+            name: <input type="text" v-model="name" @change="updateMessage('')">
+            </p>
+            <p>when are you available?</p>
+            <p>
+            start: <input type="date" v-model="start" @change="updateMessage('')" placeholder="yyyy-mm-dd">
+            end: <input type="date" v-model="end" @change="updateMessage('')" placeholder="yyyy-mm-dd">
+            </p>
+            <p><button @click="submit">click to submit</button>{{ message }}</p>
+            <p>you can add multiple intervals by submitting again</p>
+            <p>submitting a day twice makes it free</p>
+        </div>`,
+    data: function () {
+        return {
+            name: "",
+            start: "",
+            end: "",
+            message: ""
+        }
+    },
+    methods: {
+        submit: function () {
+            $.ajax({
+                type: "POST",
+                url: "/resources/php/submit.php",
+                data: {
+                    'name': this.name,
+                    'start': this.start,
+                    'end': this.end
+                },
+                success: this.updateMessage(" success")
+            });
+        },
+        updateMessage: function (mess) {
+            this.message = mess
         }
     }
 }
@@ -56,6 +105,7 @@ const routes = [
     { path: '/hello', name: 'hello', component: Hello },
     { path: '/files', name: 'files', component: Files },
     { path: '/links', name: 'links', component: Links },
+    { path: '/eventmaker', name: 'eventmaker', component: EventMaker },
     { path: '/*', name: 'notfound', component: NotFound }
 ]
 
